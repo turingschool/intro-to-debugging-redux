cfab0aeb57c83af33de4c7c6e24f69037da4dce9
Khalid Williams <khalidwill27@gmail.com>
2019-05-14 15:25:38 -0600

    Add new todo to DOM

---
+++ script.js
@@ -0,0 +1,18 @@
+var newTodoTextArea = document.querySelector('#new-todo-text');
+var addTodoBtn = document.querySelector('#new-todo-add');
+var todoList = document.querySelector('#todo-list');
+
+addTodoBtn.addEventListener('click', addTodo);
+
+
+function addTodo(event) {
+  var todoText = newTodoTextArea.value;
+  var newTodo = document.createElement('li');
+  newTodo.classList.add('list__list-item', 'todo');
+  newTodo.innerHTML = `<span class="todo__state">
+        <input class="todo__state__input todo__input" type="checkbox">
+        ${todoText}
+      </span>`
+  todoList.appendChild(newTodo);
+}
+
